eebe62320df8257659323dfe86e1492dfb8f3062
SSI Digital <42675358+ssiedu@users.noreply.github.com>
2022-05-03 16:57:55 +0530

    03052022
    Variables Program 1

---
+++ variable1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int a;       //variable declaration
+	a=10;       //variable assignment
+	int b=20;  //variable initialization
+	printf("\n value of a : %d",a);
+	printf("\n value of b : %d",b);
+	getch();
+}
